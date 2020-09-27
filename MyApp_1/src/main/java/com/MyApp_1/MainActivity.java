3cd6b98bde65c201d363fc76e5139fc8667f3d8d
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-27 12:55:42 +0300

    Урок 1. Вписать в тв из ет по подтверждению из диалогового окна

--- MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
+++ MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
@@ -1,8 +1,14 @@
 package com.MyApp_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText editText = findViewById(R.id.et);
+        Button button = findViewById(R.id.button_vizov_menu);
+        final TextView textView = findViewById(R.id.tv);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Подтвердите действие");
+                builder.setIcon(android.R.drawable.ic_dialog_info);
+                builder.setMessage("Вы уверены, что хотите изменить текст метки на новый?");
+
+                builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        System.out.println("Вы нажали \" Да\"");
+                        textView.setText(editText.getText());
+                    }
+                });
+
+                builder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        System.out.println("Вы нажали \" Нет\"");
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        });
     }
 }

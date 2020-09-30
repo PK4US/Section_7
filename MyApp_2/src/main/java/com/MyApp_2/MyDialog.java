6277dbec256e1b7936574c5fd224711eda005ff8
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-09-30 15:43:49 +0300

    Урок 2. Диалоговое окно от 0 до 10 (добавлена кнопка ок)

--- MyApp_2/src/main/java/com/MyApp_2/MyDialog.java
+++ MyApp_2/src/main/java/com/MyApp_2/MyDialog.java
@@ -2,7 +2,10 @@ package com.MyApp_2;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.fragment.app.DialogFragment;
 
 public class MyDialog extends DialogFragment {
@@ -13,6 +16,12 @@ public class MyDialog extends DialogFragment {
                 .setTitle("Ошибка!")
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setMessage("Вы ввели число не от 0 до 10")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                     public void onClick(DialogInterface dialogInterface, int i) {
+                        System.out.println("Вы нажали \" OK\"");
+                     }
+                })
                 .create();
     }
 }
